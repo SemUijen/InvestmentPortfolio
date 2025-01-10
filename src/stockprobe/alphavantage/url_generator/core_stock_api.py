27978216e1014c4961c0dc571c34d8ef977053c5
SemUijen <grootveer.sem@outlook.com>
2025-01-10 22:47:04 +0100

    Enhance TimeSeriesDailyURL model with improved docstring and type hints

--- src/stockprobe/alphavantage/url_generator/core_stock_api.py
+++ src/stockprobe/alphavantage/url_generator/core_stock_api.py
@@ -1,4 +1,6 @@
-from typing import Optional
+"""Module contains class for generating URLs for Alpha Vantage CoreStockAPI."""
+
+from __future__ import annotations
 
 from pydantic import Field
 
@@ -6,7 +8,7 @@ from .base_url import BaseAPIurl
 
 
 class TimeSeriesDailyURL(BaseAPIurl):
-    """Model for intraday time series request"""
+    """Model for intraday time series request."""
 
     function: str = "TIME_SERIES_DAILY"
-    outputsize: Optional[str] = Field("compact", description="Size of the output data")
+    outputsize: str | None = Field("compact", description="Size of the output data")
