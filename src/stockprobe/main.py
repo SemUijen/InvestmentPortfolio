a0e70017aea835f2a6ef24e4b8117254fd289f57
SemUijen <grootveer.sem@outlook.com>
2025-01-10 22:52:03 +0100

    Enhance main script with logging and improve error handling for API data fetching

--- src/stockprobe/main.py
+++ src/stockprobe/main.py
@@ -1,3 +1,8 @@
+"""Executable script to fetch stock data from the Alpha Vantage API."""
+
+from __future__ import annotations
+
+import logging
 import os
 from pprint import pprint
 
@@ -5,25 +10,30 @@ import requests
 from alphavantage.url_generator.core_stock_api import TimeSeriesDailyURL
 from dotenv import load_dotenv
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
-def get_api_data(url):
+
+def get_api_data(url) -> dict | None:
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=5)
         response.raise_for_status()  # Raise an exception for HTTP errors
         return response.json()  # Assuming the API returns JSON data
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        logging.exception("An error occurred", exc_info=e)
         return None
 
 
 if __name__ == "__main__":
 
     load_dotenv()
-    api_key = os.getenv("alphavantage_api_key")
+    api_key = os.getenv("ALPHAVANTAGE_API_KEY")
     if not api_key:
-        raise ValueError(
-            "No API key found. Please set the API_KEY environment variable."
-        )
+        error_message = "No API key found. Please set the API_KEY environment variable."
+        raise ValueError(error_message)
     url_generator = TimeSeriesDailyURL(apikey=api_key, symbol="IWDA")
     daily_url = url_generator.return_url()
     data = get_api_data(daily_url)
