a58164b0e787565279e5d51523b35fc9f9c8ca35
SemUijen <grootveer.sem@outlook.com>
2025-01-12 20:58:20 +0100

    Add unit tests for BaseAPIurl methods and symbol validation

---
+++ tests/stockprobe/alphavantage/test_base_url.py
@@ -0,0 +1,65 @@
+import pytest
+import requests_mock
+from pydantic import ValidationError
+
+from src.stockprobe.alphavantage.url_generator.base_url import BaseAPIurl, DataType
+
+
+@pytest.fixture
+def valid_base_api_url() -> BaseAPIurl:
+    """Provide a valid BaseAPIurl instance."""
+    return BaseAPIurl(
+        apikey="testapikey",
+        datatype=DataType.JSON,
+        symbol="AAPL",
+    )
+
+
+def test_to_url_params(valid_base_api_url) -> None:
+    """Test the _to_url_params method."""
+    expected = "apikey=testapikey&datatype=json&symbol=AAPL"
+    assert valid_base_api_url._to_url_params() == expected
+
+
+def test_return_url(valid_base_api_url):
+    """Test the return_url method."""
+    expected = (
+        "https://www.alphavantage.co/query?apikey=testapikey&datatype=json&symbol=AAPL"
+    )
+    print(valid_base_api_url.return_url())
+    print(expected)
+    assert valid_base_api_url.return_url() == expected
+
+
+def test_validate_symbol_success() -> None:
+    """Test that the symbol validation is successful for a valid symbol."""
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=AAPL&apikey=demo",
+            json={
+                "bestMatches": [
+                    {
+                        "1. symbol": "AAPL",
+                        "2. name": "Apple Inc.",
+                        "9. matchScore": "1.0000",
+                    },
+                ],
+            },
+            status_code=200,
+        )
+        BaseAPIurl(symbol="AAPL", apikey="demo", validate_symbol=True)
+
+
+def test_validate_symbol_no_matches_found() -> None:
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=INVALID&apikey=demo",
+            json={"bestMatches": []},
+            status_code=200,
+        )
+
+        with pytest.raises(
+            ValidationError,
+            match="Symbol INVALID not found and no close matches found",
+        ):
+            base_url = BaseAPIurl(symbol="INVALID", apikey="demo", validate_symbol=True)
