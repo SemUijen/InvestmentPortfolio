85913e2bda1b6d506be76bd996a356848a644548
SemUijen <grootveer.sem@outlook.com>
2025-01-13 21:04:40 +0100

    Add pytest test cases for TimeSeriesDailyURL class methods

--- tests/stockprobe/alphavantage/test_core_stock_api.py
+++ tests/stockprobe/alphavantage/test_core_stock_api.py
@@ -1,1 +1,26 @@
 """Pytest test cases for the BaseAPIurl class."""
+
+import pytest
+
+from src.stockprobe.alphavantage.url_generator.base_url import DataType
+from src.stockprobe.alphavantage.url_generator.core_stock_api import TimeSeriesDailyURL
+
+
+@pytest.fixture
+def url_generator() -> TimeSeriesDailyURL:
+    """Provide a valid BaseAPIurl instance."""
+    return TimeSeriesDailyURL(
+        apikey="testapikey",
+        datatype=DataType.JSON,
+        symbol="AAPL",
+        validate_symbol=False,
+    )
+
+
+def test_to_url_params(url_generator: TimeSeriesDailyURL) -> None:
+    """Test the _to_url_params method."""
+    expected = "apikey=testapikey&datatype=json&symbol=AAPL&function=TIME_SERIES_DAILY&outputsize=compact"
+    assert url_generator._to_url_params() == expected
+
+    expected = "https://www.alphavantage.co/query?apikey=testapikey&datatype=json&symbol=AAPL&function=TIME_SERIES_DAILY&outputsize=compact"
+    assert url_generator.return_url() == expected
