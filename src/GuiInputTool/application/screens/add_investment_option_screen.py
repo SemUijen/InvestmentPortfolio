85652503525bfdf4010d6604a04ccb70591e431b
SemUijen <grootveer.sem@outlook.com>
2025-05-25 14:11:24 +0200

    Add input fields to InvestmentOptionsScreen for enhanced functionality

--- src/GuiInputTool/application/screens/add_investment_option_screen.py
+++ src/GuiInputTool/application/screens/add_investment_option_screen.py
@@ -1,16 +1,24 @@
 import tkinter as tk
 from typing import TYPE_CHECKING
 
-from .base_screen import BaseScreen
+from .base_screen import BaseScreen, InputField
 
 if TYPE_CHECKING:
     from src.GuiInputTool.application.application import MainApplication
 
 
 class InvestmentOptionsScreen(BaseScreen):
-    def __init__(self, root: tk.Tk, app_controller: "MainApplication"):
+    def __init__(
+        self,
+        root: tk.Tk,
+        app_controller: "MainApplication",
+        input_fields: list[InputField],
+    ):
+        """Initialize the Investment Options screen"""
         super().__init__(root, app_controller)
 
         # Setup main frame
         self.main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
         self.main_frame.columnconfigure(1, weight=1)
+
+        self.add_input_fields(input_fields)
