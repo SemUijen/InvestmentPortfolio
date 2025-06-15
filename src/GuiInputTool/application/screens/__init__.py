7dd293df3644dace59ec5180970cf77b11176e2a
SemUijen <grootveer.sem@outlook.com>
2025-06-15 16:44:51 +0200

    Import BaseScreen in __init__.py for consistent screen management

--- src/GuiInputTool/application/screens/__init__.py
+++ src/GuiInputTool/application/screens/__init__.py
@@ -2,5 +2,6 @@ from .add_bought_io_screen import BoughtInvestmentScreen as BoughtInvestmentScre
 from .add_investment_option_screen import (
     InvestmentOptionsScreen as InvestmentOptionsScreen,
 )
+from .base_screen import BaseScreen as BaseScreen
 from .base_screen import InputField as InputField
 from .start_screen import StartupScreen as StartupScreen
