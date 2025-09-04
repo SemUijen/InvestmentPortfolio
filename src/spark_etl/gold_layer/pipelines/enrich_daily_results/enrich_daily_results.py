dfe99d64026672a86839cfa15e03b2e480da81c2
SemUijen <grootveer.sem@outlook.com>
2025-09-04 20:59:12 +0200

    feat: rename variable for clarity and enhance daily results merging logic in enrich_daily_results pipeline

--- src/spark_etl/gold_layer/pipelines/enrich_daily_results/enrich_daily_results.py
+++ src/spark_etl/gold_layer/pipelines/enrich_daily_results/enrich_daily_results.py
@@ -16,15 +16,14 @@ def main():
     io_value_overtime = InvestmentOptionValueOvertime(spark)
 
     # Gold Tables
-    fact_table = FactDailyResult(spark)
+    fact_table_daily = FactDailyResult(spark)
 
     # Steps:
     ## 1: Get Latest date for each symbol in fact table
-    df_fact_daily = fact_table.get_dataframe()
+    df_fact_daily = fact_table_daily.get_dataframe()
     df_fact_daily = df_fact_daily.groupBy("symbol").agg(
         F.max("date").alias("latest_date"),
     )
-
     ## 2: Get IOs Bought sincs latest date
     df_io_bought = io_bought.get_dataframe()
     if not df_fact_daily.isEmpty():
@@ -38,14 +37,78 @@ def main():
             "left",
         ).select(df_io_bought["*"])
 
-    df_new_daily_results = df_io_bought.select(
+    df_new_io_bought = df_io_bought.select(
         "symbol",
         F.col("date_bought").alias("date"),
+        F.col("price").alias("price"),
+        F.col("amount").alias("number_owned"),
+        F.col("cost_of_buy"),
     )
-    df_new_daily_results.show()
+
+    df_new_io_bought.show()
     ## 3 Create new rows for facttable from latest date until yesterday for each symbol
 
-    ### 3.1
+    df_io_value_overtime = io_value_overtime.get_dataframe()
+    df_io_value_overtime.show()
+
+    df_new_daily_results = (
+        df_new_io_bought.select(
+            F.col("symbol"),
+            (F.col("price") * F.col("number_owned")).alias("total_investment"),
+            F.col("number_owned"),
+            F.col("cost_of_buy"),
+            F.explode(
+                F.sequence(
+                    F.col("date"),
+                    F.lit(F.current_date()),
+                    F.expr("interval 1 day"),
+                ),
+            ).alias("date"),
+        )
+        .groupBy("date", "symbol")
+        .agg(
+            F.sum("total_investment").alias("total_investment"),
+            F.sum("number_owned").alias("number_owned"),
+            F.first("cost_of_buy").alias("cost_of_buy"),
+        )
+        .orderBy("date", "symbol")
+    )
+    df_new_daily_results.show()
+
+    df_new_daily_results = (
+        df_new_daily_results.join(
+            df_io_value_overtime,
+            (df_new_daily_results.symbol == df_io_value_overtime.symbol)
+            & (df_new_daily_results.date == df_io_value_overtime.date),
+            "left",
+        )
+        .drop(
+            df_io_value_overtime["date"],
+        )
+        .select(
+            df_new_daily_results["*"],
+            F.date_format(F.col("date"), "yyyyddMM").cast("int").alias("date_id"),
+            F.col("close").alias("value_single_io"),
+            (F.col("total_investment") / F.col("number_owned")).alias("avg_buy_price"),
+            (F.col("close") * F.col("number_owned")).alias("total_value"),
+            (F.col("total_value") - F.col("total_investment")).alias(
+                "product_profit",
+            ),
+            (F.lit(0)).alias(
+                "currency_profit",
+            ),  # Should be 0 for euros and currency eschange rates for others to the euro
+            (
+                F.col("product_profit")
+                + F.col("currency_profit")
+                - 2 * F.col("cost_of_buy")
+            ).alias("total_profit"),
+        )
+    ).orderBy("date", "symbol")
+
+    fact_table_daily.merge_dataframe(df_new_daily_results)
+
+    fact_table_daily.get_dataframe().show()
+
     spark.stop()
 
 
