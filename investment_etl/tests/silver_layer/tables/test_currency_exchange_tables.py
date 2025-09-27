f9ec32d11f1c24f90e17ad24c79f060af35323cb
SemUijen <grootveer.sem@outlook.com>
2025-09-27 16:08:42 +0200

    Refactor test files: Add missing patches for DeltaLakeTable and SparkTable in test cases

--- investment_etl/tests/silver_layer/tables/test_currency_exchange_tables.py
+++ investment_etl/tests/silver_layer/tables/test_currency_exchange_tables.py
@@ -1,10 +1,12 @@
 import pytest
-
+from unittest.mock import patch
 from investment_etl.silver_layer.tables import (
     CurrencyExchangeRateDeltaLake,
     CurrencyExchangeRateSpark,
 )
-
+from investment_etl.utils import DeltaLakeTable, SparkTable
+@patch.object(DeltaLakeTable, "_create_table_if_not_exists", return_value=None)
+@patch.object(SparkTable, "_create_table_if_not_exists", return_value=None)
 class TestCurrencyExchangeTable:
     def test_similarity_between_deltalake_and_spark(self, deltalake_vs_spark_tables_test, spark_session):
         spark_table = CurrencyExchangeRateSpark(spark=spark_session)
